594647aa08269ced73f17d9aa3c2af57f32c79b0
Ryanu01 <ryangomes08090109@gmail.com>
2025-10-07 11:34:53 +0530

    add URL shortener functionality with MongoDB model

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { nanoid } from 'nanoid';
 import dotenv from 'dotenv';
 import connectDB from './src/config/mongo.config.js';
+import urlSchema from './src/config/models/shorturl.model.js';
 
 dotenv.config("./.env");
 const app = express();
@@ -12,9 +13,25 @@ app.use(express.urlencoded({extended: true}));
 
 app.post('/api/create', (req, res) => {
     const {url} = req.body;
+    const shortUrl = nanoid(7);
+    const newUrl = new urlSchema({
+        full_url: url,
+        short: shortUrl
+    })
+    newUrl.save()
     res.send(nanoid(7))    
 })
 
+
+app.get('/:shortUrl', async(req, res) => {
+    const {shortUrl} = req.params;
+    const url = await urlSchema.findOne({short: shortUrl});
+    if(url) {
+        res.redirect(url.full_url);
+    }else {
+        res.status(404).send("Not Found");
+    }
+})
 
 app.listen(PORT, () => {
     connectDB();
