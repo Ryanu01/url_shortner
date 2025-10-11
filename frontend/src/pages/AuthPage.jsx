eb8d06b281af29bc06b0e23bf88ef5588c32e80f
Ryanu01 <ryangomes08090109@gmail.com>
2025-10-11 22:54:58 +0530

    impelmented register Form ui

--- frontend/src/pages/AuthPage.jsx
+++ frontend/src/pages/AuthPage.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import LoginForm from "../components/loginForm";
+import RegisterForm from "../components/registerForm";
 
 const AuthPage = () => {
+    const [login, setLogin] = useState(true);
+
     return (
         <div className="min-h-screen bg-gray-100 flex flex-col items-center justiyf-centre p-4">
-            <LoginForm />
+            {login ? <LoginForm state={setLogin} /> : <RegisterForm state={setLogin} />} 
         </div>
     )
 }
