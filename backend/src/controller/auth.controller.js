40e6b1f0aa91e5d66dff7060f53f9bd35b83a223
Ryanu01 <ryangomes08090109@gmail.com>
2025-10-12 14:55:05 +0530

    added password encryption and changed userSchmea added select false for password

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -5,14 +5,17 @@ import wrapAsync from "../utils/trycatchwrapper.js";
 
 export const register_user = wrapAsync( async (req, res) => {
     const {name, email, password} = req.body;
-    const token = await registerUser(name, email, password);
+    const {token, user} = await registerUser(name, email, password);
+    req.user = user;
     res.cookie("accessToken", token, cookieOptions);
-    res.status(200).json("Login success");
+    res.status(200).json("Register success");
 })
 
 export const login_user = wrapAsync( async (req, res) => {
     const {email, password} = req.body;
     const {token, user} = await loginUser(email, password);
+
+    req.user = user;
     res.cookie("accessToken", token, cookieOptions);
-    res.status(200).json({message: "Login success"});
+    res.status(200).json({user: user, message: "Login success"});
 })
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+import bcrypt from "bcrypt"
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -13,8 +13,27 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,
+        select: false,
     }
 });
+
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.set('toJSON', {
+    transform: function (doc, ret) {
+        delete ret.password;
+        delete ret.__v;
+        return ret;
+    }
+})
+
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+})
 
 const User = mongoose.model("User", userSchema);
 
