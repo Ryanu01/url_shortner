53c18f24e70f96d4cdff03ab57f5ecab2397b43b
Ryanu01 <ryangomes08090109@gmail.com>
2025-10-12 14:56:09 +0530

    implelemted seperate password and email verification

--- backend/src/services/auth.service.js
+++ backend/src/services/auth.service.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
-import {findUserByEmail, createUser} from "../dao/user.dao.js";
+import {findUserByEmail, createUser, findUserByEmailAndPassword} from "../dao/user.dao.js";
 import { ConflictError } from "../utils/errorHandler.js";
 import { signToken } from "../utils/helper.js";
 
@@ -9,13 +9,14 @@ export const registerUser = async (name, email, password) => {
     if(user) throw new ConflictError("User already exists");
     const newUser = await createUser(name, email, password);
     const token =  signToken({id: newUser._id});
-    return token;
+    return {token, user};
 }
 
 export const loginUser = async (email, password) => {
-    const user = await findUserByEmail(email);
-
-    if(!user || user.password !== password) throw new Error("Invalid Credentails");
+    const user = await findUserByEmailAndPassword(email);
+    if(!user ) throw new Error("Invalid email or password");
+    const isPasswordValid = await user.comparePassword(password)
+    if(!isPasswordValid) throw new Error("Invalid password") 
     const token = signToken({id: user._id});
     return {token, user};
 }
